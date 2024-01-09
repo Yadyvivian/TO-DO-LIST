745c4d549bff431397aa2d40e91316297ef3bc1c
Yadyvivian <148440973+Yadyvivian@users.noreply.github.com>
2024-01-09 14:52:45 +0100

    change in html and js

--- js/check-in.js
+++ js/check-in.js
@@ -5,34 +5,67 @@ function directlyToAnotherPage() {
     }
 
 
-    // Esperar a que el contenido HTML se cargue
+  
 document.addEventListener('DOMContentLoaded', function() {
-  // Obtener el formulario y sus elementos
+  
   const form = document.querySelector('form');
   const usuarioInput = document.getElementById('Usuario');
   const passwordInput = document.getElementById('password1');
 
-  // Verificar si ya hay un usuario almacenado
+  
   const usuarioGuardado = localStorage.getItem('usuario');
   if (usuarioGuardado) {
       usuarioInput.value = usuarioGuardado;
   }
 
-  // Manejar el envío del formulario
+  
   form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Evitar el envío del formulario
+      event.preventDefault(); 
 
       const usuario = usuarioInput.value;
       const password = passwordInput.value;
 
-      // Validar el usuario y la contraseña (puedes implementar tu lógica de validación aquí)
+      
       if (usuario.trim() !== '' && password.trim() !== '') {
-          // Guardar el usuario en el almacenamiento local
+         
           localStorage.setItem('usuario', usuario);
-          // Redirigir a otra página o realizar otra acción después de iniciar sesión
+         
           window.location.href = 'edit.html';
       } else {
           alert('Por favor, introduce un usuario y contraseña válidos.');
       }
   });
 });
+
+
+const nodemailer = require('nodemailer');
+
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail', 
+  auth: {
+    user: 'tucorreo@gmail.com', 
+    pass: 'tupassword'
+  }
+});
+
+
+const enviarCorreo = (correoDestino, mensaje) => {
+  const mailOptions = {
+    from: 'tucorreo@gmail.com',
+    to: correoDestino,
+    subject: 'Restablecer contraseña',
+    text: mensaje
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Correo enviado: ' + info.response);
+    }
+  });
+};
+
+
+enviarCorreo('correoDestino@example.com', 'Haz clic en el enlace para restablecer tu contraseña.');
